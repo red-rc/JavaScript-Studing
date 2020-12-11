f832a8a850ae386fb7aca3481f2e0316a5d19057
red-rc-rc <avira222217@gmail.com>
2020-12-11 15:13:48 +0200

    init

--- scripts.js
+++ scripts.js
@@ -1,31 +1,24 @@
-/*var secondsInAMinute = 60;
-var minutesInAnHour = secondsInAMinute * 60;
-var hoursInADay = minutesInAnHour * 24;
-var daysInAYear = hoursInADay * 365;
-var age = daysInAYear * 11;
-console.log(age);
-
-var balloons = 100;
-balloons /= 4;
-console.log(balloons);
-
-var string = "hElLo tHerE, I aM kEnObi!";
-string[0].toUpperCase() + string.slice(1).toLowerCase();
-console.log(string);
-*/
 var calling = [
     "Helen you are bitch", 
     "Yeees, YOU are bitch", 
     "You how sand!", 
     "Beach", 
     "Hey Hilan!", 
-    "You are the bitch, like piss of sand in anakin's shoes", 
+    "You are the bitch, like peace of sand in anakin's shoes", 
     "Bye bitch!"
+    "I hate you in all realitis!"
+    "I think Dragon egg is game of shit"
+    "But Dragon egg have tasty egg"
+    "Dragon shit"
+    "ANakin don't play in dragon"
+    "Star Wars better Dragon age"
+    "Dragon socks be shit!"
+    "I am KENOBIII! Hello there!"
 ];
 
 document.getElementById('button').onclick = function() {   
     let callingField = document.getElementById('calling-field');
     let out = '';
-    out += `<p class="alertName" role="alert">${calling[Math.floor(Math.random() * 7)]}</p>`;
+    out += `<p class="alertName" role="alert">${calling[Math.floor(Math.random() * 15)]}</p>`;
     callingField.innerHTML = out;
 }
